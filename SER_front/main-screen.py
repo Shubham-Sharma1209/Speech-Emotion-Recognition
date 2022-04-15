0e433b9d7cb71c1f2c75439130b2ea43b36f5c8d
Shubham-Sharma1209 <sharma.shubh1209@gmail.com>
2022-04-15 18:58:03 +0530

    FIleChooser added

--- SER_front/main-screen.py
+++ SER_front/main-screen.py
@@ -1,61 +1,46 @@
-import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.properties import StringProperty
-from kivy.config import Config
-from kivy.uix.textinput import TextInput
-from kivy.metrics import dp
-from kivy.graphics import Rectangle,Color
-from kivy.lang import Builder
-from kivy.core.text import LabelBase
-from file_selector import FileApp
-from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
+from kivy.lang.builder import Builder 
+from kivy.config import Config
+from kivy.properties import StringProperty,ObjectProperty
+from kivy.core.text import LabelBase    
+from kivy.uix.popup import Popup
 
 Config.set('graphics','resizable',True)
-        
-    
-class MainWidget(Widget):
-    f_path=StringProperty()
-    addr=StringProperty()
-    Show_Emo=StringProperty()
-    Show_Emo="NO EMOTION"
-    addr=""+"NO FILE SELECTED"
-    def display_add(self):
-        try:      
-            # f_path=fs.path
-            # print(f_path) 
-            self.addr=show_popup()
-            if self.addr=='':
-                self.addr='Cannot Load Files'
+class Main_screen(FloatLayout):
+    file_path=StringProperty('Currently No files')
+
+    def call_adrr(self):
+        try:
+            self.the_popup = FileChoosePopup(load=self.load)
+            self.the_popup.open()
         except:
-            self.addr="Cannot locate files"
-        x=TextInput(text=self.addr,size_hint=(0.5,0.5),size=(dp(220),dp(40)),disabled=True,pos=(dp(420),dp(460)))
-        self.add_widget(x)
+            self.file_path='Cannot Select files'
+
+    def load(self, selection):
+        self.file_path = str(selection[0])
+        self.the_popup.dismiss()
+        # print(self.file_path)
+
+        # if self.file_path:
+        #     self.ids.get_file.text = self.file_path
+
     def emo(self):
         self.Show_Emo="Cant detect emotion: No files"
         return self.Show_Emo
 
+class FileChoosePopup(Popup):
+    load = ObjectProperty()
 
-def show_popup():
-    show = Filechooser()
-    popupWindow = Popup(title ="Select a file",content =show,size =(400, 400),pos=(0,0))
+
+
+class SER(App):
+    def build(self):
+        return Main_screen()
     
-    popupWindow.open()
-    print(show.f_path)
-    return show.f_path
-
-class Filechooser(FloatLayout): 
-	def select(self, *args):
-		try: 
-			self.label.text = args[1][0]
-		except: pass
-
-class front_pageApp(App):
-    pass
-
 LabelBase.register(name='Georgia',fn_regular='Georgia Regular font.ttf')
+LabelBase.register(name='Georgia Bold',fn_regular='georgia bold.ttf')    
 Builder.load_file('front_page.kv')
+if __name__=='__main__':
+    SER().run()
 
-front_pageApp().run()   
